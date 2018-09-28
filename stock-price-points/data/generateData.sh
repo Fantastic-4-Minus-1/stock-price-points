e364fcb3802665dd3bf063aab7ed460f2fd548e0
Jenn <huynhjennt@gmail.com>
2018-09-28 11:15:01 -0700

    consolidate data gen scripts into file

---
+++ stock-price-points/data/generateData.sh
@@ -0,0 +1,4 @@
+(node data/dataFileGenA-C.js & node data/dataFileGenD-F.js) &&
+  (node data/dataFileGenG-I.js & node data/dataFileGenJ-L.js) &&
+  (node data/dataFileGenM-O.js & node data/dataFileGenP-R.js) &&
+  (node data/dataFileGenS-V.js)
