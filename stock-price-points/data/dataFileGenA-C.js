55574be0cac12628386704236a7b4039c36cfcfb
Jenn <huynhjennt@gmail.com>
2018-09-27 19:45:28 -0700

    add separate data gen files

---
+++ stock-price-points/data/dataFileGenA-C.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { alphabet } = require('./idGenerator');
+const { assembleTestData } = require('./dataGenerator');
+
+const letters = alphabet.slice(0, 3);
+
+letters.forEach(letter => {
+  let wstream = fs.createWriteStream(`./data/seed/data${letter}.json`);
+  const dataSet = assembleTestData([letter], wstream, letter);
+});
