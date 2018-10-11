3e9a704d7e86d2fa255d334fe7b0b047a0ef1dde
Jenn <huynhjennt@gmail.com>
2018-10-11 12:36:38 -0700

    clean up server index

--- stock-price-points/server/app.js
+++ stock-price-points/server/app.js
@@ -1,5 +1,4 @@
 // require('newrelic');
-const redis = require('redis');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -7,12 +6,6 @@ const logger = require('morgan');
 
 const app = express();
 const router = require('./routes');
-
-const client = redis.createClient(6379, 'ec2-54-153-88-58.us-west-1.compute.amazonaws.com');
-
-client.on('error', function (err) {
-  console.log('Error ' + err);
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
